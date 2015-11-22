44aec0fc17b6d20fac53f671315d542367cff799
kexline <kexline@@users.noreply.github.com>
2015-11-22 17:29:30 -0600

    Generic Pair tuple - kx

---
+++ src/Pair.java
@@ -0,0 +1,11 @@
+
+// Utility class for presenting pairs of stars to be connected in asterisms.
+
+public class Pair<T> {
+	
+    T p1, p2;
+    Pair(T p1, T p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+}
