e9ac264c10d18b7247badbc40f1738c451d39606
kexline <kexline@@users.noreply.github.com>
2015-11-24 11:02:37 -0600

    Constellation, continued - kx

--- src/Constellation.java
+++ src/Constellation.java
@@ -15,6 +15,9 @@ public class Constellation {
 	boolean zodiac;					// is it a member of the zodiac?
 	double rasc; 					// right ascension
 	double decl; 					// declination
+	double ascNodeLongi;	 		// ascending node longitude
+	double meanLongi;		 		// mean longitude
+	double hourAngle;				// hour angle
 	ArrayList<Pair<Integer>> ast;	// Unbounded list of pairs of StarIDs	
 	
 	public Constellation(String s) {
@@ -49,16 +52,60 @@ public class Constellation {
 	private ArrayList<Pair<Integer>> stringToPairs(String s) {
 		ArrayList<Pair<Integer>> a = new ArrayList<Pair<Integer>>();
 		
-
     	String delimiter = " ";
         String[] tokens = s.split(delimiter);
-        for (int i=0; i<length(tokens); i++) {
+        for (int i=0; i<tokens.length; i++) {
         	
         }
-		
 		
 		return a;
 	}
 	
+	public String getName() {
+		return this.name;
+	}
+	
+	public boolean inZodiac() {
+		return this.zodiac;
+	}
+	
+	public String getAbbreviation() {
+		if (this.shortName != null ) { return this.shortName; }
+		else { return ""; }
+	}
+	
+	public ArrayList<Pair<Integer>> getAsterisms() {
+		return this.ast;
+	}
+	
+	// Ascending node longitude is not guaranteed to be set.
+	// Since 0 is a valid value, will default to 190 instead.
+	
+	public double getAscendingNodeLongitude() {
+		return this.ascNodeLongi; 
+	}
+	// Mean longitude is not guaranteed to be set.
+	// Since 0 is a valid value, will default to 500 instead.
+	public double getMeanLongitude() {
+		return this.meanLongi;
+	}
+	
+	// hourAngle  is not guaranteed to be set.
+	// Since 0 is a valid value, will default to 500 instead.
+	public double getHourAngle() {
+		return hourAngle;
+	}
+
+	public void setHourAngle(double ha) {
+		this.hourAngle=ha;
+	}
+	
+	public void setMeanLongitude(double ml) {
+		this.meanLongi=ml;
+	}
+	
+	public void setAscendingNodeLongitude(double an) {
+		this.ascNodeLongi=an;
+	}
 	
 }
