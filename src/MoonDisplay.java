9df4c45137d1890098f3d38d6984de6feae8d084
kexline <kexline@@users.noreply.github.com>
2015-11-30 23:10:47 -0600

    name & outline for moon - kx

--- src/MoonDisplay.java
+++ src/MoonDisplay.java
@@ -22,11 +22,13 @@ public class MoonDisplay extends PlanetDisplay {
         try {
             image = ImageIO.read(getClass().getResource("/images/Moon/" + icon));
             int size = 40;
+            g.setColor(Color.white);
+            g.drawArc((int) (grid_x + scroll_x - size/2), (int) (grid_y + scroll_y - size/2 -1), size+1, size+1, 0, 360);
             g.drawImage(image, (int) (grid_x + scroll_x - size/2), (int) (grid_y + scroll_y - size/2), size, size, null);
-            // Maybe remove label?
-            /*g.setColor(Color.magenta);
-            if (show_labels == true && label != null) {
-                g.drawString(label, (int) (grid_x + scroll_x + 2), (int) (grid_y + scroll_y - 2));}*/
+            if (tag != null) {
+                g.setColor(Color.cyan);
+                g.drawString(tag, (int) (grid_x + scroll_x -15), (int) (grid_y + scroll_y +35));
+                }
         } catch (IOException ex) {
             Logger.getLogger(MessierDisplay.class.getName()).log(Level.SEVERE, null, ex);
         }
